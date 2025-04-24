18c574c7065343b3f0f0180d8067f8404d9912b7
Patricie34 <patricie.johnova@email.cz>
2025-04-24 07:02:58 +0200

    file removed

--- run.sh
+++
@@ -1,3 +0,0 @@
-# Run the Nextflow process using kuberun
-/storage1/shared_resources/nf/build/releases/nextflow-24.04.4-all kuberun xsvato01/nanopore_k8s/main.nf -r main -head-image 'cerit.io/nextflow/nextflow:24.04.4' \
--resume -with-report -params-file samplesheet.json -c my_local_nextflow.config
